85ba3ecb25d67d1d248c08e445912581ffb586b2
Thien Nguyen <ngkhanhthien94@gmail.com>
2022-09-13 15:06:23 +0700

    :alembic: update test running on headed mode

--- test_playwright_homepage.py
+++ test_playwright_homepage.py
@@ -1,21 +1,10 @@
-import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import expect
 
 
-def test_homepage_has_Playwright_in_title_and_get_started_link_linking_to_the_intro_page(page: Page):
-    page.goto("https://playwright.dev/")
-
-    # Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Playwright"))
-
-    # create a locator
-    get_started = page.locator("text=Get Started")
-
-    # Expect an attribute "to be strictly equal" to the value.
-    expect(get_started).to_have_attribute("href", "/docs/intro")
-
-    # Click the get started link.
-    get_started.click()
-
-    # Expects the URL to contain intro.
-    expect(page).to_have_url(re.compile(".*intro"))
+def test_playwright_homepage(playwright):
+    chromium = playwright.chromium  # or "firefox" or "webkit".
+    browser = chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://playwright.dev/python/")
+    expect(page).to_have_title("Fast and reliable end-to-end testing for modern web apps | Playwright Python")
+    browser.close()
